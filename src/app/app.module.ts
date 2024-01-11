2dfbf2c15c4171ddb4218ef0bce35244d7e494db
BatuhanEvirgen <batuhanevirgen@hotmail.com>
2024-01-11 18:44:18 +0300

    Update files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,35 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModelComponent } from './components/model/model.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { LocalStorageService } from './local-storage.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModelComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatDividerModule,
+    BrowserAnimationsModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    LocalStorageService
+
   ],
   bootstrap: [AppComponent]
 })
